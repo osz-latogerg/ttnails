7b06cae22da79ebed3ec3cad3256f5915b6c2ad9
Gergely Lato <gergely.lato@oce.com>
2015-10-06 17:37:00 +0200

    Added flex-slider, css changes

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('ttnails', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap']);
+    .module('ttnails', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap', 'angular-flexslider']);
 
 })();
