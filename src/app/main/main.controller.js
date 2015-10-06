4fc1cb6d055bd4c3f6148497bb03cabdc056af2e
Gergely Lato <gergely.lato@oce.com>
2015-10-06 16:10:28 +0200

    Create menu and page structure

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   'use strict';
 
   angular
@@ -11,10 +11,34 @@
 
     vm.myInterval = 0;
     vm.noWrapSlides = false;
-    vm.slides = {
-      main: true,
-      nails: false,
-      makeup: false
-    };
+    vm.slides = [{
+      template: 'app/components/home/home.html',
+      cssClass: 'slide-main'
+    }, {
+      template: 'app/components/nails/nails.html',
+      cssClass: 'slide-nails'
+    }, {
+      template: 'app/components/makeup/makeup.html',
+      cssClass: 'slide-makeup'
+    }, {
+      template: 'app/components/prices/prices.html',
+      cssClass: 'slide-prices'
+    }, {
+      template: 'app/components/contact/contact.html',
+      cssClass: 'slide-contact'
+    }];
+
+    function goToSlide(index) {
+        for (var i = 0; i < vm.slides.length; i++) {
+            var slide = vm.slides[i];
+            if (index === i) {
+                slide.active = true;
+            } else {
+                slide.active = false;
+            }
+        }
+    }
+
+    vm.goToSlide = goToSlide;
   }
 })();
