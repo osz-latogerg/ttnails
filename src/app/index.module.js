9a2dd2faf4c565714f28b533db41b83a5fcccde2
Gergely Lato <gergely.lato@oce.com>
2015-10-07 17:44:48 +0200

    added slick slider to experiment

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('ttnails', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap', 'angular-flexslider']);
+    .module('ttnails', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap', 'angular-flexslider', 'slick']);
 
 })();
